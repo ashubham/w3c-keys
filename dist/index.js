648c0fc28caa284daa0ab7a0ab8829bac5008fbb
Jun Wooram <wooram@flex.team>
2021-01-29 11:59:52 +0900

    [1.0.3] Bump Version

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ export var Key;
     Key["Alt"] = "Alt";
     Key["CapsLock"] = "CapsLock";
     Key["Escape"] = "Escape";
+    Key["Esc"] = "Esc";
     Key["Space"] = " ";
     Key["PageUp"] = "PageUp";
     Key["PageDown"] = "PageDown";
